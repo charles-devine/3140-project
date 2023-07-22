b1fa1a338a996ad1538813580d68b6c1ad03101a
Charles <77303846+charles-devine@users.noreply.github.com>
2023-07-22 17:52:10 -0400

    updated tests

--- static_code_analysis.py
+++ static_code_analysis.py
@@ -1,3 +1,18 @@
 import subprocess
-file_to_analyze = "main.py"
+file_to_analyze = "breast_cancer_prediction.py"
 subprocess.run(["pylint", file_to_analyze])
+
+# [Done] exited with code=0 in 1.357 seconds
+
+# [Running] python -u "c:\Users\charl\Desktop\3140-project\static_code_analysis.py"
+
+# ************* Module breast_cancer_prediction
+# breast_cancer_prediction.py:1:0: C0114: Missing module docstring (missing-module-docstring)
+# breast_cancer_prediction.py:12:26: E1101: Instance of 'tuple' has no 'data' member (no-member)
+# breast_cancer_prediction.py:12:62: E1101: Instance of 'tuple' has no 'feature_names' member (no-member)
+# breast_cancer_prediction.py:13:22: E1101: Instance of 'tuple' has no 'target' member (no-member)
+
+# ------------------------------------------------------------------
+# Your code has been rated at 4.67/10
+
+# [Done] exited with code=0 in 5.782 seconds
---
+++ test_breast_cancer_prediction.py
@@ -0,0 +1,56 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+import sklearn.datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+class BreastCancerPredictionTests(unittest.TestCase):
+
+    def setUp(self):
+        breast_cancer_dataset = sklearn.datasets.load_breast_cancer()
+
+        self.data_frame = pd.DataFrame(breast_cancer_dataset.data, columns=breast_cancer_dataset.feature_names)
+        self.data_frame['label'] = breast_cancer_dataset.target
+
+        self.X = self.data_frame.drop(columns='label', axis=1)
+        self.Y = self.data_frame['label']
+
+        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(self.X, self.Y, test_size=0.2, random_state=2)
+        self.model = LogisticRegression(max_iter=1000)
+        self.model.fit(self.X_train, self.Y_train)
+
+    def test_data_preprocessing(self):
+        self.assertEqual(self.data_frame.shape[0], 569)
+        self.assertEqual(self.data_frame.shape[1], 31)
+
+    def test_model_training(self):
+        self.assertIsNotNone(self.model)
+
+    def test_accuracy_on_training_data(self):
+        X_train_prediction = self.model.predict(self.X_train)
+        training_data_accuracy = accuracy_score(self.Y_train, X_train_prediction)
+        self.assertGreaterEqual(training_data_accuracy, 0.0)
+        self.assertLessEqual(training_data_accuracy, 1.0)
+
+    def test_accuracy_on_test_data(self):
+        X_test_prediction = self.model.predict(self.X_test)
+        test_data_accuracy = accuracy_score(self.Y_test, X_test_prediction)
+        self.assertGreaterEqual(test_data_accuracy, 0.0)
+        self.assertLessEqual(test_data_accuracy, 1.0)
+
+    def test_prediction(self):
+        input_data = (15.34, 14.26, 102.5, 704.4, 0.1073, 0.2135, 0.2077, 0.09756, 0.2521, 0.07032,
+                      0.4388, 0.7096, 3.384, 44.91, 0.006789, 0.05328, 0.06446, 0.02252, 0.03672,
+                      0.004394, 18.07, 19.08, 125.1, 980.9, 0.139, 0.5954, 0.6305, 0.2393, 0.4667, 0.09946)
+
+        input_data_as_numpy_array = np.asarray(input_data)
+        input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+
+        prediction = self.model.predict(input_data_reshaped)
+        self.assertIn(prediction[0], [0, 1])
+
+if __name__ == '__main__':
+    unittest.main()
